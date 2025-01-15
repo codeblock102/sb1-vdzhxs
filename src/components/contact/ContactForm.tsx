91127bd3d47d235d6c1bdc0469d6d40d80f4a176
codeblock102 <63685618+codeblock102@users.noreply.github.com>
2025-01-15 17:31:21 -0500

    changed the classname

--- src/components/contact/ContactForm.tsx
+++ src/components/contact/ContactForm.tsx
@@ -32,7 +32,7 @@ export default function ContactForm() {
   };
 
   return (
-    <form name = "contact-form" onSubmit={handleSubmit} className="space-y-6" netlify>
+    <form name ="contact-form" onSubmit={handleSubmit} className="space-y-6" data-netlify="true" >
       <div>
         <label htmlFor="name" className="block text-[#00274D] mb-2">Name</label>
         <input
